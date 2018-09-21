0fbe299194f58e4b883ba49308298b57ac340cdf
Youssouf Traore <youssoufj@gmail.com>
2018-09-21 18:59:56 +0200

    Updating

---
+++ Countdown.java
@@ -0,0 +1,10 @@
+public class Countdown {
+    public static void countdown(int n) {
+    if (n == 0) {
+    System.out.println("Blastoff!");
+} else {
+    System.out.println(n);
+    countdown(n - 1);
+}
+}
+}
